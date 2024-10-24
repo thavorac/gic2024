c6a35cb65a9556038a191edb91df8fa5117400d6
Thavorac <thavorac@sala.co>
2024-10-24 08:42:11 +0700

    TP1: Finish step 3
       * Init vuejs project
       * Clean unused code
       * Add message "This is my first Vuejs Project"

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  This is my first VueJs project
+  <RouterView />
+</template>
+
+<style scoped></style>
