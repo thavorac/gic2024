705c59408f0a318163ecf94c965805f2db725707
Thavorac <thavorac@sala.co>
2024-11-01 08:27:49 +0700

    Init state management project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>State management with Pinia</div>
+  <div style="display: flex; gap: 10px">
+    <div>
+      Component A: <br />
+      <ComponentA />
+    </div>
+    <div>
+      Component B: <br />
+      <ComponentB />
+    </div>
+  </div>
+</template>
+
+<script>
+import ComponentA from "./components/ComponentA.vue";
+import ComponentB from "./components/ComponentB.vue";
+export default {
+  components: { ComponentA, ComponentB },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/ComponentB.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    {{ count }}
+    <br /><br /><br />
+    <button @click="decreaseCount">Decrease</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    decreaseCount() {
+      this.count--;
+    },
+  },
+};
+</script>
+<style>
+.container {
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+</style>
