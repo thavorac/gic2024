a9b2361f22fd8c14bced353ecd77b6622bb2d0f4
Thavorac <thavorac@sala.co>
2024-11-13 14:20:37 +0700

    Upload image in category api

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,6 +1,29 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CategoriesService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
+import * as multer from 'multer';
+
+const multerOptions: MulterOptions = {
+  storage: multer.diskStorage({
+    destination: './uploads',
+    filename: (req, file, callback) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      callback(null, uniqueSuffix + '-' + file.originalname);
+    },
+  }),
+  limits: {
+    fileSize: 1 * 1024 * 1024, // Limit file size to 1MB
+  },
+};
 
 @Controller('api/categories')
 export class CategoriesController {
@@ -12,8 +35,16 @@ export class CategoriesController {
   }
 
   @Post()
-  createCategory(@Body() body: CreateCategoryDto): any {
-    const category = this.categoryService.create(body);
+  @UseInterceptors(FileInterceptor('image', multerOptions))
+  createCategory(
+    @UploadedFile() image: Express.Multer.File,
+    @Body() body: CreateCategoryDto,
+  ): any {
+    console.log('Image:', image);
+    const category = this.categoryService.create({
+      ...body,
+      image: image.path,
+    });
     return category;
   }
 }
