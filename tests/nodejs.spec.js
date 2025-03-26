86e7bacccc80c393f98884b88da3531240b0032b
Thavorac <thavorac@sala.co>
2025-03-26 09:02:43 +0700

    Test fill in search bar [Failed]

--- tests/nodejs.spec.js
+++ tests/nodejs.spec.js
@@ -19,3 +19,24 @@ test("get started link", async ({ page }) => {
     page.getByRole("heading", { name: "Introduction to Node.js" })
   ).toBeVisible();
 });
+
+test("go to Live Debugging page with search bar", async ({ page }) => {
+  await page.goto("https://nodejs.org/en");
+
+  // Visit page learn
+  await page.getByRole("link", { name: "Learn", exact: true }).click();
+
+  // Click on search button
+  await page.getByRole("button", { name: "Start typing..." }).click();
+
+  // Type in the search bar.
+  await page.fill("input[type='search']", "Live Debugging");
+
+  // Click the search button.
+  await page.getByRole("link", { name: "Live Debugging", exact: true }).click();
+
+  // Expects page to have a heading with the name of Live Debugging.
+  await expect(
+    page.getByRole("heading", { name: "Live Debugging" })
+  ).toBeVisible();
+});
