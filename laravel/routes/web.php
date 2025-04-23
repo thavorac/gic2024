bfc3f4a819f89948573ae8f9863345787ce0f6e3
Thavorac <thavorac@sala.co>
2025-04-23 14:10:09 +0700

    Feature upload files

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +19,8 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/upload_file', function () {
+    return view('upload_file');
+});
+Route::post('/upload', [UploadController::class, 'upload'])->name('upload');
--- laravel/tests/Feature/CategoryTest.php
+++ laravel/tests/Feature/CategoryTest.php
@@ -9,7 +9,15 @@ use Tests\TestCase;
 class CategoryTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Description: Check if we can access the get all categories api
+     * Precondition: None
+     * Test Steps: 1. Hit the get all categories api
+     *             2. Check if the response status is 200
+     * Test Data : None
+     * Expected Result: The response status should be 200
+     * Actual Result: The response status is 200
+     * Status: Passed
+     * Remark: None
      */
     public function test_if_we_can_access_get_all_categories_api(): void
     {
