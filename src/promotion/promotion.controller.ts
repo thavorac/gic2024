117423a266d429061de1996e64357d1a14e2f3d8
Thavorac <thavorac.chun@gmail.com>
2025-11-01 10:22:31 +0700

    Add update and delete promotion

--- src/promotion/promotion.controller.ts
+++ src/promotion/promotion.controller.ts
@@ -1,7 +1,10 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  Param,
+  Patch,
   Post,
   UploadedFile,
   UseInterceptors,
@@ -11,6 +14,7 @@ import { CreatePromotionDto } from './dto/create-promotion.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import * as multer from 'multer';
+import { UpdatePromotionDto } from './dto/update-promotion.dto';
 
 const multerOptions: MulterOptions = {
   storage: multer.diskStorage({
@@ -46,5 +50,25 @@ export class PromotionsController {
       image: image.path,
     });
     return promotion;
+  }
+
+  @Patch(':id')
+  @UseInterceptors(FileInterceptor('image', multerOptions))
+  updatePromotion(
+    @Param('id') id: string,
+    @UploadedFile() image: Express.Multer.File,
+    @Body() body: Partial<UpdatePromotionDto>,
+  ): any {
+    console.log('image', image);
+    return this.promotionService.update(id, {
+      ...body,
+      image: image.path,
+    });
+  }
+
+  @Delete(':id')
+  deletePromotion(@Param('id') id: string): any {
+    const result = this.promotionService.delete(id);
+    return result;
   }
 }
--- src/promotion/promotion.service.ts
+++ src/promotion/promotion.service.ts
@@ -18,4 +18,24 @@ export class PromotionsService {
   findAll() {
     return this.promotionRepository.findAll();
   }
+  update(id: string, updatePromotionDto: Partial<UpdatePromotionDto>) {
+    const result = this.promotionRepository.update(updatePromotionDto as any, {
+      where: { id },
+      returning: true,
+    });
+    if (!result) {
+      throw new Error('Promotion not found');
+    }
+    return 'Update success';
+  }
+
+  delete(id: string) {
+    const result = this.promotionRepository.destroy({
+      where: { id },
+    });
+    if (!result) {
+      throw new Error('Promotion not found');
+    }
+    return 'Delete success';
+  }
 }
